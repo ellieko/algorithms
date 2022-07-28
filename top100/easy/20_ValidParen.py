f3e4e334a3780e21254482e15e66e8c841808d3c
Ellie Ko <ellieko0712@gmail.com>
2022-07-28 13:54:46 -0500

    [neetcode] stack probs

--- top100/easy/20_ValidParen.py
+++ top100/easy/20_ValidParen.py
@@ -10,7 +10,22 @@ An input string is valid if:
 '''
 
 class Solution:
-    def isValid(self, s: str) -> bool:
+    # time complexity: O(n)
+    # space complexity: O(n)
+    def isValid_v1(self, s: str) -> bool:
+        d = {'(': ')', '{': '}', '[': ']'}
+        stack = []
+        for c in s:
+            if c in d:
+                stack.append(d[c])
+            else:
+                if len(stack) == 0 or c != stack.pop():
+                    return False
+        return len(stack) == 0
+
+    # time complexity: O(n)
+    # space complexity: O(n)
+    def isValid_v2(self, s: str) -> bool:
         hash = {")":"(", "]":"[", "}":"{"}
         stack = []
         for char in s:
